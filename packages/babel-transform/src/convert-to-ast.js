8358b87c32a000401952eb6f2db40e7892eec297
Marek Magdziak <marek.magdziak@hotmail.cz>
2020-02-02 15:53:37 +0000

    Create package

--- packages/babel-transform/src/convert-to-ast.js
+++
@@ -1,8 +0,0 @@
-import * as acorn from 'acorn';
-import jsx from 'acorn-jsx';
-
-const parser = acorn.Parser.extend(jsx());
-
-export default function convertToAst(code) {
-    return parser.parse(code, { sourceType: 'module' });
-}
